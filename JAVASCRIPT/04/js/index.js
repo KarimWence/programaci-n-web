d8f40ec6a1b3bf719a252de3e6a55dbc07df4e2b
Karim <kwence_22@alu.uabcs.mx>
2024-10-27 23:10:07 -0700

    Avance

--- JAVASCRIPT/04/js/index.js
+++ JAVASCRIPT/04/js/index.js
@@ -1,5 +1,76 @@
 const addButton = document.getElementById("button-add");
 const textInput = document.getElementById("input-text");
+const textTaskList = document.getElementById("tasks-list");
+var historyList = [];
 
-const li = document.getElementById("li-id");
-console.log(li);
+addButton.addEventListener("click", function() {
+    if(!textInput.value == "") {
+        createElements(textInput.value);
+    } else {
+        console.log("Error");
+    }
+});
+
+function createElements(text) {
+    const newTask = document.createElement("li");
+    const textTask = document.createElement("p");
+    const checkTask = document.createElement("input");
+    const deleteButton = document.createElement("input");
+    deleteButton.type = "button";
+    deleteButton.classList.add("button-delete");
+    deleteButton.value = "Eliminar";
+    checkTask.type = "checkbox";
+    textTask.textContent = text;
+    textTask.classList.add("text-task");
+    newTask.appendChild(checkTask);
+    newTask.appendChild(textTask);
+    newTask.appendChild(deleteButton);
+    addRemoveFunction();
+    checkTask.onclick = checkboxChecked;
+    addToTaskList(newTask);
+    saveAtHistory(text);
+}
+
+function addRemoveFunction() {
+    const deleteButtons = document.getElementsByClassName("button-delete");
+    Array.from(deleteButtons).forEach((removeButton) => {
+        removeButton.addEventListener("click", () => {
+            removeButton.parentElement.remove();
+        })
+    })
+}
+
+function addToTaskList(newTask) {
+    textTaskList.appendChild(newTask);
+}
+
+function saveAtHistory(element) {
+    
+    console.log(historyList);
+    historyList.push(element);
+    localStorage.setItem("history", JSON.stringify(historyList));
+}
+
+function updateHistory() {
+    historyList = JSON.parse(localStorage.getItem("history"));
+    while(textTaskList.lastElementChild) {
+        textTaskList.removeChild(textTaskList.lastElementChild);
+    }
+    /*historyList.forEach(element => {
+        createElements(element);
+    });*/
+    console.log(historyList);
+}
+
+function checkboxChecked() {
+    if(this.checked == true) {
+        this.nextElementSibling.setAttribute("style", "text-decoration: line-through; color: rgba(0, 0, 0, 0.4);");
+    } else {
+        this.nextElementSibling.setAttribute("style", "text-decoration: none; color: black;");
+    }
+}
+
+
+window.onload = function() {
+    updateHistory();
+}
