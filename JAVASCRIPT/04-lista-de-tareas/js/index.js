b71f389282e6ed2bc7107654044bc99d556fed1d
Karim Wence <karim.wence@vectralis.com>
2024-10-29 09:06:04 -0700

    Ejercicio #4 - Lista de tareas

--- JAVASCRIPT/04-lista-de-tareas/js/index.js
+++ JAVASCRIPT/04-lista-de-tareas/js/index.js
@@ -1,17 +1,15 @@
 const addButton = document.getElementById("button-add");
 const textInput = document.getElementById("input-text");
 const textTaskList = document.getElementById("tasks-list");
-var historyList = [];
 
 addButton.addEventListener("click", function() {
     if(!textInput.value == "") {
         createElements(textInput.value);
-    } else {
-        console.log("Error");
+        saveAtHistory();
     }
 });
 
-function createElements(text) {
+function createElements(text, hasDecoration) {
     const newTask = document.createElement("li");
     const textTask = document.createElement("p");
     const checkTask = document.createElement("input");
@@ -22,55 +20,80 @@ function createElements(text) {
     checkTask.type = "checkbox";
     textTask.textContent = text;
     textTask.classList.add("text-task");
+    if(hasDecoration) {
+        textTask.style.textDecoration = "line-through";
+        textTask.style.color = "rgba(0, 0, 0, 0.4)";
+        checkTask.checked = true;
+    }
     newTask.appendChild(checkTask);
     newTask.appendChild(textTask);
     newTask.appendChild(deleteButton);
-    addRemoveFunction();
     checkTask.onclick = checkboxChecked;
     addToTaskList(newTask);
-    saveAtHistory(text);
 }
 
 function addRemoveFunction() {
-    const deleteButtons = document.getElementsByClassName("button-delete");
+    const deleteButtons = document.querySelectorAll(".button-delete");
     Array.from(deleteButtons).forEach((removeButton) => {
-        removeButton.addEventListener("click", () => {
+        removeButton.addEventListener("click", e => {
             removeButton.parentElement.remove();
+            saveAtHistory();
         })
     })
 }
 
 function addToTaskList(newTask) {
     textTaskList.appendChild(newTask);
+    textInput.value = "";
+    addRemoveFunction();
+    saveAtHistory();
 }
 
-function saveAtHistory(element) {
-    
-    console.log(historyList);
-    historyList.push(element);
-    localStorage.setItem("history", JSON.stringify(historyList));
+function saveAtHistory() {
+    let history = [];
+    let hasDecoration = []; 
+    const textTask = document.querySelectorAll(".text-task");
+    if(textTask.length != 0) {
+        textTask.forEach(element => {
+            history.push(element.textContent);
+            hasDecoration.push(element.style.textDecoration === "line-through");
+            localStorage.setItem("history", JSON.stringify(history));
+            localStorage.setItem("hasDecoration", JSON.stringify(hasDecoration));
+        });
+    } else {
+        localStorage.clear();
+    }
 }
 
 function updateHistory() {
-    historyList = JSON.parse(localStorage.getItem("history"));
+    let history = [];
+    let hasDecoration = [];
     while(textTaskList.lastElementChild) {
         textTaskList.removeChild(textTaskList.lastElementChild);
     }
-    /*historyList.forEach(element => {
-        createElements(element);
-    });*/
-    console.log(historyList);
+    if(history && hasDecoration) {
+        history = JSON.parse(localStorage.getItem("history"));
+        hasDecoration = JSON.parse(localStorage.getItem("hasDecoration"));
+        history.forEach((element, line) => {
+        createElements(element, hasDecoration[line]);
+    });
+    }
 }
 
 function checkboxChecked() {
     if(this.checked == true) {
         this.nextElementSibling.setAttribute("style", "text-decoration: line-through; color: rgba(0, 0, 0, 0.4);");
+        saveAtHistory();
     } else {
         this.nextElementSibling.setAttribute("style", "text-decoration: none; color: black;");
+        saveAtHistory();
     }
 }
 
 
 window.onload = function() {
-    updateHistory();
+    let history = JSON.parse(localStorage.getItem("history"));
+    if(history) {
+        updateHistory();
+    }
 }
