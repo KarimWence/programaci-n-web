1dc4d633265ec540993f60294a77ea986bb3a826
Karim <kwence_22@alu.uabcs.mx>
2024-11-20 23:51:37 -0700

    Ejercicio con servidor

---
+++ PHP/examen-sitio-web/header/header.php
@@ -0,0 +1,47 @@
+<header>
+        <nav>
+            <div class="top-actions-container">
+                <div class="styled-container logo-container">
+                    <img src="../img/logo-no-background.png" alt="logo">
+                </div>
+                <div class="styled-container browser-container">
+                    <input type="search" name="" id="browser" placeholder="Buscar...">
+                </div>
+                <div class="styled-container user-menu-container">
+                    <img src="../img/usuario.png" alt="">
+                </div>
+            </div>
+            <input type="checkbox" name="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <i class="fa-solid fa-bars"></i>
+            </label>
+            <div class="links-container-father">
+                <ul class="options">
+                    <li class="links-container">
+                        <a class="main-link" href="#">Blog</a>
+                    </li>
+                    <li class="links-container">
+                        <a class="main-link" href="#">Recetas</a>
+                        <ul class="submenu-recipes">
+                            <li><a href="#">Las mejores valoradas</a></li>
+                            <li><a href="#">Gourmet en casa</a></li>
+                            <li><a href="#">Vegetarianas</a></li>
+                            <li><a href="#">Bajo nivel calórico</a></li>
+                        </ul>
+                    </li>
+                    <li class="links-container">
+                        <a class="main-link" href="../pages/page1.php">Consejos</a>
+                    </li>
+                    <li class="links-container">
+                        <a class="main-link" href="#">Tutoriales</a>
+                    </li>
+                    <li class="links-container">
+                        <a class="main-link" href="#">Recursos</a>
+                    </li>
+                    <li class="links-container">
+                        <a class="main-link" href="#">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
